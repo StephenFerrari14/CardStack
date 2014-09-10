cde8f4d46411cee1395d748a4b4c59767471cc0d
Stephen Ferrari <stferrar@student.umass.edu>
2014-09-10 13:45:51 -0400

    Update

--- src/CardStack.java
+++ src/CardStack.java
@@ -23,8 +23,10 @@ public class CardStack {
  
 	public static void main(String[] args) {
         //1 Clubs, 2 Diamonds, 3 Hearts, 4 Spades
-        //Naming scheme is 101 for ace of clubs
+        //Naming scheme is 101 for Ace of Clubs, 413 for King of Spades
         //Generate the deck, maybe change it to generate a list of card objects?
+        //
+
         ArrayList<Integer> deck = new ArrayList<Integer>(52);
         for(int x = 1; x <= 4; x++){
             for(int y = 1; y <= 13; y++){
@@ -32,14 +34,22 @@ public class CardStack {
             }
         }
 
-
+        ArrayList<Card> DeckofCards = new ArrayList<Card>(52);
         CardStack cd = new CardStack(deck);
+
+        for(int x = 0; x < 52; x++){
+            Card c = cd.generateCard(deck.get(x));
+            DeckofCards.add(c);
+        }
 
         Collections.shuffle(cd.Deck);
         //cd.printDeck(cd.Deck);
-        int picked = cd.pickCard(cd.Deck);
-        Card firstCard = cd.generateCard(picked);
-        System.out.println(firstCard.getCardName() + " of " + firstCard.getCardSuit());
+        //for(int i = 0; i < cd.Deck.size(); i++)
+         //   System.out.println(cd.Deck.get(i));
+        printDeck(DeckofCards);
+        //int picked = cd.pickCard(cd.Deck);
+        //Card firstCard = cd.generateCard(picked);
+        //System.out.println(firstCard.getCardName() + " of " + firstCard.getCardSuit());
 				
 	}
 
@@ -52,16 +62,16 @@ public class CardStack {
 
     public Card generateCard(int cV){
         int cardVal = cV % 100;
-        String cardsuit = SUITS[(int)(cV/100)];
-        String cardname = cardName[cV % 100];
+        String cardsuit = SUITS[((int)(cV/100))-1];
+        String cardname = cardName[(cV % 100)-1];
         Card card = new Card(cardVal,cardname,cardsuit);
         return card;
     }
 	
-	public void printDeck(ArrayList<Integer> deck){
+	public static void printDeck(ArrayList<Card> deck){
 		//Print out deck
 		for(int i = 0; i < deck.size(); i++)
-			System.out.println(deck.get(i));
+			System.out.println(deck.get(i).getCardName() + " " + deck.get(i).getCardSuit());
 	}
 	
 	
