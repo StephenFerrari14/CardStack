bc5e9f36be4afdffad1bd2afd06ae524dd613f8c
Stephen Ferrari <stferrar@student.umass.edu>
2014-09-10 20:25:02 -0400

    Talbe

---
+++ src/Table.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+
+public class Table {
+
+	//Get it? Table because thats where you play card games
+
+	//Table will have game type, scores, players,
+	private String gametype;
+	private int score;
+	private ArrayList<Player> playerList;
+	
+	public Table(int score){
+		this.score = score;
+	}
+	
+	public void setScore(int score){
+		this.score = score;
+	}
+	
+	public int getScore(){
+		return score;
+	}
+
+}
